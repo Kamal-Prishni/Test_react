8a9801f091c6fb0194702d4b0fbaf36a256341cf
Kamal-Prishni <kamal@prishni.in>
2026-02-03 11:25:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/home/")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("API error");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        setError("Failed to load data");
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Django → React Test</h2>
+
+        {message && <p>{message}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
